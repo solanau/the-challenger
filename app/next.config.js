28950e3e48d803003c1d98ff5dd993e1175f0283
Andrés Guerrero <andresmgsl@gmail.com>
2022-11-17 19:34:41 +0000

    feat: deploy added

--- app/next.config.js
+++ app/next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
     reactStrictMode: true,
     images: {
         domains: ['avatars.githubusercontent.com', 'raw.githubusercontent.com'],
+        unoptimized: true
     },
     experimental: {
         images: {
