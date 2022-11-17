c09e7b9381b911e2ec9c59e9569b9219fadb123f
Andrés Guerrero <andresmgsl@gmail.com>
2022-11-17 21:59:20 +0000

    chore: cleaning!

--- app/next.config.js
+++ app/next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
     reactStrictMode: true,
     images: {
         domains: ['avatars.githubusercontent.com', 'raw.githubusercontent.com'],
-        unoptimized: true
     },
     experimental: {
         images: {
