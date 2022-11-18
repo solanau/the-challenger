c0d7aedbb71573a3a855357a997228efb3e9ee31
Valentin Madrid <valentin@incility.com>
2022-11-18 17:04:57 +0100

    🍉 - Add way to filter challenges

--- app/src/pages/challenges/index.tsx
+++ app/src/pages/challenges/index.tsx
@@ -4,6 +4,7 @@ import PendingChallengesSection from 'components/challenges-page/pending-challen
 import { useChallenges } from 'hooks/use-challenges';
 import { NextPage } from 'next';
 import { NextSeo } from 'next-seo';
+import { ChangeEvent, useMemo, useState } from 'react';
 import {
     isActiveChallenge,
     isExpiredChallenge,
@@ -14,6 +15,20 @@ const ChallengesPage: NextPage = () => {
     const challenges = useChallenges(
         process.env.NEXT_PUBLIC_HEAVY_DUTY_BOUNTY_API_EVENT_ID,
     );
+    const [selectedCategory, setSelectedCategory] = useState<string>('');
+    const filteredChallenges = useMemo(() => {
+        // Avoid filter when selectedCategory is null
+        if (!selectedCategory) {
+            return challenges;
+        }
+        return challenges.filter(
+            challenge => challenge.type === selectedCategory,
+        );
+    }, [selectedCategory, challenges]);
+
+    const handleCategoryChange = (event: ChangeEvent<HTMLSelectElement>) => {
+        setSelectedCategory(event.target.value);
+    };
 
     return (
         <>
@@ -23,19 +38,47 @@ const ChallengesPage: NextPage = () => {
             ></NextSeo>
 
             {challenges.length > 0 ? (
-                <div className="flex w-full flex-row flex-wrap gap-5 bg-gradient-to-tr from-primary to-secondary p-5 sm:p-8 md:px-16 lg:px-32 lg:py-16 xl:px-48 xl:py-20">
-                    <ActiveChallengesSection
-                        challenges={challenges.filter(isActiveChallenge)}
-                    />
+                <>
+                    <div>
+                        <select
+                            name="category-list"
+                            id="category-list"
+                            onChange={handleCategoryChange}
+                            className="rounded-md border-2 border-white bg-transparent px-4 py-2 text-white"
+                        >
+                            <option value="">All</option>
+                            <option value="Social">Social</option>
+                            <option value="Video">Video</option>
+                            <option value="Concept">Concept</option>
+                            <option value="Wallet">Wallet</option>
+                            <option value="SDK">SDK</option>
+                            <option value="Deploy">Deploy</option>
+                            <option value="Staking">Staking</option>
+                            <option value="Game">Game</option>
+                            <option value="Client">Client</option>
+                            <option value="NFT">NFT</option>
+                        </select>
+                    </div>
+                    <div className="flex w-full flex-row flex-wrap gap-5 bg-gradient-to-tr from-primary to-secondary p-5 sm:p-8 md:px-16 lg:px-32 lg:py-16 xl:px-48 xl:py-20">
+                        <ActiveChallengesSection
+                            challenges={filteredChallenges.filter(
+                                isActiveChallenge,
+                            )}
+                        />
 
-                    <PendingChallengesSection
-                        challenges={challenges.filter(isPendingChallenge)}
-                    />
+                        <PendingChallengesSection
+                            challenges={filteredChallenges.filter(
+                                isPendingChallenge,
+                            )}
+                        />
 
-                    <ExpiredChallengesSection
-                        challenges={challenges.filter(isExpiredChallenge)}
-                    />
-                </div>
+                        <ExpiredChallengesSection
+                            challenges={filteredChallenges.filter(
+                                isExpiredChallenge,
+                            )}
+                        />
+                    </div>
+                </>
             ) : (
                 <div className="flex h-20 items-center justify-center">
                     <p className="text-secondary">No challenges found.</p>
