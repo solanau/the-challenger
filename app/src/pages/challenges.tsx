815edc3c5adf382bc3d7c8e4646081b69484b5c7
Valentin Madrid <valentin@incility.com>
2022-11-18 16:58:20 +0100

    🍉 - Add possibility to filter challenges

--- app/src/pages/challenges.tsx
+++ app/src/pages/challenges.tsx
@@ -7,6 +7,7 @@ import { GetServerSideProps, NextPage } from 'next';
 import { unstable_getServerSession } from 'next-auth';
 import { NextSeo } from 'next-seo';
 import { authOptions } from 'pages/api/auth/[...nextauth]';
+import { useMemo, useState } from 'react';
 import { ChallengeView } from 'types/challenge';
 import {
     isActiveChallenge,
@@ -19,36 +20,74 @@ type ChallengesPageProps = {
     challenges: ChallengeView[];
 };
 
-const ChallengesPage: NextPage<ChallengesPageProps> = ({ challenges }) => (
-    <>
-        <NextSeo
-            title="Solana Bounty Challenges"
-            description="Complete the Solana challenges to collect Solana rewards!"
-        ></NextSeo>
+const ChallengesPage: NextPage<ChallengesPageProps> = ({ challenges }) => {
+    const [selectedCategory, setSelectedCategory] = useState();
+    function getFilteredList() {
+        // Avoid filter when selectedCategory is null
+        if (!selectedCategory) {
+            return challenges;
+        }
+        return challenges.filter(
+            challenge => challenge.type === selectedCategory,
+        );
+    }
+    var filteredList = useMemo(getFilteredList, [selectedCategory, challenges]);
 
-        {/* <p>Event Pubkey: x</p> */}
+    const handleCategoryChange = e => {
+        setSelectedCategory(e.target.value);
+    };
+    return (
+        <>
+            <NextSeo
+                title="Solana Bounty Challenges"
+                description="Complete the Solana challenges to collect Solana rewards!"
+            ></NextSeo>
 
-        {challenges.length > 0 ? (
-            <div className="flex w-full flex-row flex-wrap gap-5 bg-gradient-to-tr from-primary to-secondary p-5 sm:p-8 md:px-16 lg:px-32 lg:py-16 xl:px-48 xl:py-20">
-                <ActiveChallengesSection
-                    challenges={challenges.filter(isActiveChallenge)}
-                />
+            {/* <p>Event Pubkey: x</p> */}
 
-                <PendingChallengesSection
-                    challenges={challenges.filter(isPendingChallenge)}
-                />
+            {challenges.length > 0 ? (
+                <div className="flex w-full flex-row flex-wrap gap-5 bg-gradient-to-tr from-primary to-secondary p-5 sm:p-8 md:px-16 lg:px-32 lg:py-16 xl:px-48 xl:py-20">
+                    <div>
+                        <select
+                            name="category-list"
+                            id="category-list"
+                            onChange={handleCategoryChange}
+                            className="rounded-md border-2 border-white bg-transparent px-4 py-2 text-white"
+                        >
+                            <option value="">All</option>
+                            <option value="Social">Social</option>
+                            <option value="Video">Video</option>
+                            <option value="Concept">Concept</option>
+                            <option value="Wallet">Wallet</option>
+                            <option value="SDK">SDK</option>
+                            <option value="Deploy">Deploy</option>
+                            <option value="Staking">Staking</option>
+                            <option value="Game">Game</option>
+                            <option value="Client">Client</option>
+                            <option value="NFT">NFT</option>
+                        </select>
+                    </div>
 
-                <ExpiredChallengesSection
-                    challenges={challenges.filter(isExpiredChallenge)}
-                />
-            </div>
-        ) : (
-            <div className="flex h-20 items-center justify-center">
-                <p className="text-secondary">No challenges found.</p>
-            </div>
-        )}
-    </>
-);
+                    <ActiveChallengesSection
+                        challenges={filteredList.filter(isActiveChallenge)}
+                    />
+
+                    <PendingChallengesSection
+                        challenges={filteredList.filter(isPendingChallenge)}
+                    />
+
+                    <ExpiredChallengesSection
+                        challenges={filteredList.filter(isExpiredChallenge)}
+                    />
+                </div>
+            ) : (
+                <div className="flex h-20 items-center justify-center">
+                    <p className="text-secondary">No challenges found.</p>
+                </div>
+            )}
+        </>
+    );
+};
 
 export default ChallengesPage;
 
