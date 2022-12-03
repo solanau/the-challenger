66952b2d7478b15dee1d6a5057da93c2f13ecce8
Dan M <danielarturomt@gmail.com>
2022-12-03 19:00:40 +0000

    feat: improve pages layout

--- app/src/pages/challenges/index.tsx
+++ app/src/pages/challenges/index.tsx
@@ -1,6 +1,26 @@
 /* eslint-disable react/no-unescaped-entities */
+import Button from 'components/common/button';
+import Text from 'components/common/text';
 import { NextPage } from 'next';
+import { TbPlus } from 'react-icons/tb';
 
-const ChallengesPage: NextPage = () => <>challenges</>;
+const ChallengesPage: NextPage = () => (
+    <>
+        <div className="flex w-full flex-col gap-5 bg-gradient-to-tr from-primary to-secondary p-5 sm:p-8 md:px-16 lg:px-32 lg:py-16 xl:px-48 xl:py-20">
+            <Text variant="big-heading">Challenges</Text>
+
+            <Text variant="paragraph">
+                Explore challenges available for you to use in your next event.
+            </Text>
+
+            <Button
+                icon={TbPlus}
+                text={'Create a challenge'}
+                variant="transparent"
+                className="bg-zinc-700"
+            ></Button>
+        </div>
+    </>
+);
 
 export default ChallengesPage;
--- app/src/pages/events/index.tsx
+++ app/src/pages/events/index.tsx
@@ -1,6 +1,28 @@
 /* eslint-disable react/no-unescaped-entities */
+import Button from 'components/common/button';
+import Text from 'components/common/text';
 import { NextPage } from 'next';
+import { TbPlus } from 'react-icons/tb';
 
-const EventsPage: NextPage = () => <>events</>;
+const EventsPage: NextPage = () => (
+    <>
+        <div className="flex w-full flex-col flex-wrap gap-5 bg-gradient-to-tr from-primary to-secondary p-5 sm:p-8 md:px-16 lg:px-32 lg:py-16 xl:px-48 xl:py-20">
+            <Text variant="big-heading">Events</Text>
+            <Text variant="paragraph">
+                Explore events near by and start competing with other students
+                for the grand prize.
+            </Text>
+
+            <div>
+                <Button
+                    icon={TbPlus}
+                    text={'Create an event'}
+                    variant="transparent"
+                    className="bg-zinc-700"
+                ></Button>
+            </div>
+        </div>
+    </>
+);
 
 export default EventsPage;
