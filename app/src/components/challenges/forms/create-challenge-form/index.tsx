293626d8edf050ea4a87e5057fed0ef8036c637f
jpcaulfi <joe.caulfield@solana.org>
2022-12-05 18:59:42 -0600

    shuffling/deleting files: components

--- app/src/components/challenges/forms/create-challenge-form/index.tsx
+++ app/src/components/challenges/forms/create-challenge-form/index.tsx
@@ -1,34 +1,44 @@
 import Button from 'components/common/button';
 import Card from 'components/common/card';
 import { useFormik } from 'formik';
-import { EditChallengePayload } from 'types/challenge';
+import { createChallenge } from 'lib/api/challenge';
+import { SetStateAction } from 'react';
+import { CreateChallengePayload } from 'types/challenge';
 
 interface ChallengeFormData {
     title: string;
     description: string;
 }
 
-interface EditChallengeFormProps {
-    data?: ChallengeFormData;
-    onSubmit(data?: EditChallengePayload): void;
+interface CreateChallengeFormProps {
+    setIsCreateChallengeModalOpen(value: SetStateAction<boolean>): void;
 }
 
-const EditChallengeForm = ({ data, onSubmit }: EditChallengeFormProps) => {
+const CreateChallengeForm = (props: CreateChallengeFormProps) => {
     const formik = useFormik({
-        initialValues: data ?? {
+        initialValues: {
             title: '',
             description: '',
+            tags: '',
         },
-        onSubmit: value => {
-            onSubmit(value);
+        onSubmit: data => {
+            handleCreateChallenge(data);
         },
     });
+    const handleCreateChallenge = (
+        createChallengePayload: CreateChallengePayload,
+    ) => {
+        props.setIsCreateChallengeModalOpen(false);
+        createChallenge(createChallengePayload)
+            .then(() => alert('Challenge created!'))
+            .catch(error => alert(error));
+    };
 
     return (
         <form onSubmit={formik.handleSubmit}>
             <div className="pt-4">
                 <label
-                    htmlFor="event-title"
+                    htmlFor="challenge-title"
                     className="block w-full border-none bg-transparent py-2 outline-none"
                 >
                     Title
@@ -36,19 +46,19 @@ const EditChallengeForm = ({ data, onSubmit }: EditChallengeFormProps) => {
 
                 <Card className="h-fit w-full p-5 transition-all duration-300 focus-within:border-3 focus-within:border-primary">
                     <input
-                        id="event-title"
+                        id="challenge-title"
                         name="title"
                         maxLength={32}
                         className="w-full bg-transparent outline-none"
                         onChange={formik.handleChange}
-                        placeholder="Enter a title for the event"
+                        placeholder="Enter a title for the challenge"
                     />
                 </Card>
             </div>
 
             <div className="pt-4">
                 <label
-                    htmlFor="event-description"
+                    htmlFor="challenge-description"
                     className="block w-full border-none bg-transparent py-2 outline-none"
                 >
                     Description
@@ -56,13 +66,34 @@ const EditChallengeForm = ({ data, onSubmit }: EditChallengeFormProps) => {
 
                 <Card className="h-fit w-full p-5 transition-all duration-300 focus-within:border-3 focus-within:border-primary">
                     <textarea
-                        id="event-description"
+                        id="challenge-description"
                         name="description"
                         className="w-full bg-transparent outline-none"
                         maxLength={500}
                         rows={4}
                         onChange={formik.handleChange}
-                        placeholder="Enter a description for the event"
+                        placeholder="Enter a description for the challenge"
+                    />
+                </Card>
+            </div>
+
+            <div className="pt-4">
+                <label
+                    htmlFor="challenge-tags"
+                    className="block w-full border-none bg-transparent py-2 outline-none"
+                >
+                    Tags
+                </label>
+
+                <Card className="h-fit w-full p-5 transition-all duration-300 focus-within:border-3 focus-within:border-primary">
+                    <textarea
+                        id="challenge-tags"
+                        name="tags"
+                        className="w-full bg-transparent outline-none"
+                        maxLength={500}
+                        rows={4}
+                        onChange={formik.handleChange}
+                        placeholder="Enter some comma-separated tags for this challenge"
                     />
                 </Card>
             </div>
@@ -79,4 +110,4 @@ const EditChallengeForm = ({ data, onSubmit }: EditChallengeFormProps) => {
     );
 };
 
-export default EditChallengeForm;
+export default CreateChallengeForm;
---
+++ app/src/components/events/forms/create-event-form/index.tsx
@@ -0,0 +1,127 @@
+import Button from 'components/common/button';
+import Card from 'components/common/card';
+import { useFormik } from 'formik';
+import { createNewEvent } from 'lib/api/event';
+import { SetStateAction } from 'react';
+import { CreateEventPayload } from 'types/event';
+
+interface CreateEventFormProps {
+    setIsCreateEventModalOpen(value: SetStateAction<boolean>): void;
+}
+
+const CreateEventForm = (props: CreateEventFormProps) => {
+    const formik = useFormik({
+        initialValues: {
+            title: '',
+            description: '',
+            location: '',
+            date: '',
+        },
+        onSubmit: data => {
+            handleCreateEvent(data);
+        },
+    });
+
+    const handleCreateEvent = (createEventPayload: CreateEventPayload) => {
+        props.setIsCreateEventModalOpen(false);
+        createNewEvent(createEventPayload)
+            .then(() => alert('Event created!'))
+            .catch(error => alert(error));
+    };
+
+    return (
+        <form onSubmit={formik.handleSubmit}>
+            <div className="pt-4">
+                <label
+                    htmlFor="event-title"
+                    className="block w-full border-none bg-transparent py-2 outline-none"
+                >
+                    Title
+                </label>
+
+                <Card className="h-fit w-full p-5 transition-all duration-300 focus-within:border-3 focus-within:border-primary">
+                    <input
+                        id="event-title"
+                        name="title"
+                        maxLength={32}
+                        className="w-full bg-transparent outline-none"
+                        onChange={formik.handleChange}
+                        placeholder="Enter a title for the event"
+                    />
+                </Card>
+            </div>
+
+            <div className="pt-4">
+                <label
+                    htmlFor="event-description"
+                    className="block w-full border-none bg-transparent py-2 outline-none"
+                >
+                    Description
+                </label>
+
+                <Card className="h-fit w-full p-5 transition-all duration-300 focus-within:border-3 focus-within:border-primary">
+                    <textarea
+                        id="event-description"
+                        name="description"
+                        className="w-full bg-transparent outline-none"
+                        maxLength={500}
+                        rows={4}
+                        onChange={formik.handleChange}
+                        placeholder="Enter a description for the event"
+                    />
+                </Card>
+            </div>
+
+            <div className="pt-4">
+                <label
+                    htmlFor="event-location"
+                    className="block w-full border-none bg-transparent py-2 outline-none"
+                >
+                    Location
+                </label>
+
+                <Card className="h-fit w-full p-5 transition-all duration-300 focus-within:border-3 focus-within:border-primary">
+                    <input
+                        id="event-location"
+                        name="location"
+                        maxLength={32}
+                        className="w-full bg-transparent outline-none"
+                        onChange={formik.handleChange}
+                        placeholder="Enter the event's location (ie. City, Country)"
+                    />
+                </Card>
+            </div>
+
+            <div className="pt-4">
+                <label
+                    htmlFor="event-date"
+                    className="block w-full border-none bg-transparent py-2 outline-none"
+                >
+                    Date
+                </label>
+
+                <Card className="h-fit w-full p-5 transition-all duration-300 focus-within:border-3 focus-within:border-primary">
+                    <input
+                        id="event-date"
+                        name="date"
+                        maxLength={32}
+                        className="w-full bg-transparent outline-none"
+                        onChange={formik.handleChange}
+                        placeholder="Enter the event's start date: MM-DD-YYYY"
+                    />
+                </Card>
+            </div>
+
+            <div className="width-full flex flex-row justify-end gap-2 pt-4">
+                <Button
+                    className="w-40"
+                    type="submit"
+                    variant="orange"
+                    text="Submit"
+                />
+            </div>
+        </form>
+    );
+};
+
+export default CreateEventForm;
