f9b53565064c6b0217beb7509d088c49541cffc7
Marco Ordoñez <marco@evolbit.net>
2023-05-26 11:43:46 +0200

    WIP

---
+++ backend/functions/src/util/metaplex.ts
@@ -0,0 +1,25 @@
+import { Metaplex, bundlrStorage, keypairIdentity } from "@metaplex-foundation/js";
+import { Connection, Keypair, PublicKey } from "@solana/web3.js";
+
+
+export const mintToUser = async (metaplex: Metaplex, keypair: Keypair, userAddress: string, candyMachineAddress: string) => {
+
+    const candyMachine = await metaplex
+        .candyMachines()
+        .findByAddress({ address: new PublicKey(candyMachineAddress) });
+
+    return metaplex.candyMachines().mint({
+        candyMachine,
+        owner: new PublicKey(userAddress),
+        collectionUpdateAuthority: new PublicKey('AJBbXVqxBAhLHsQvasXnn58aJTjZixKeAsW1KnPeraDs')
+    }, { commitment: 'finalized', payer: keypair })
+}
+
+export const initializeMetaplex = (cluster: string, keypair: Keypair) => {
+    const connection = new Connection(cluster);
+    return Metaplex.make(connection)
+        .use(keypairIdentity(keypair))
+        .use(bundlrStorage({
+            address: 'https://devnet.bundlr.network', // remove this to use main
+        }));
+}
