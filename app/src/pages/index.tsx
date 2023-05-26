da47c471ff61784f96849a97d6a8bd4a59333791
Marco Ordoñez <marco@evolbit.net>
2023-05-26 11:44:09 +0200

    WIP

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,48 +1,58 @@
 import Button from 'components/common/button';
 import Text from 'components/common/text';
+import { sendCertificates } from 'lib/api';
 import { NextPage } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
 import { TbSearch } from 'react-icons/tb';
 
 // Home
-const Home: NextPage = () => (
-    <>
-        <Head>
-            <title>The Challenger </title>
-        </Head>
-        <div className="flex">
-            <div className="relative z-30 mt-28 flex h-full w-full flex-col items-center justify-evenly gap-16 px-4 pt-20 text-left sm:mt-20 sm:items-start sm:px-8 md:px-16 lg:mt-0 lg:px-32 xl:px-64">
-                <Text className="font-bold" variant="super-hero">
-                    The challenger
-                </Text>
+const Home: NextPage = () => {
 
-                <div className="max-w-[650px]">
-                    <Text variant="paragraph">
-                        Welcome hackers to the bounty challenge competition
+    const handleSendCertificates = () => {
+        console.log('handleSendCertificates')
+        sendCertificates('f9c8a945-25c4-48cc-bf7f-b982fd6de31c')
+    }
+
+    return (
+        <>
+            <Head>
+                <title>The Challenger </title>
+            </Head>
+            <div className="flex">
+                <div className="relative z-30 mt-28 flex h-full w-full flex-col items-center justify-evenly gap-16 px-4 pt-20 text-left sm:mt-20 sm:items-start sm:px-8 md:px-16 lg:mt-0 lg:px-32 xl:px-64">
+                    <Text className="font-bold" variant="super-hero">
+                        The challenger
                     </Text>
 
-                    <Text variant="paragraph">Explore all our challenges</Text>
-                </div>
+                    <div className="max-w-[650px]">
+                        <Text variant="paragraph">
+                            Welcome hackers to the bounty challenge competition
+                        </Text>
 
-                <Link
-                    href={{
-                        pathname: '/events',
-                    }}
-                    passHref
-                >
-                    <a className="flex-1 sm:flex-none">
-                        <Button
-                            icon={TbSearch}
-                            text={'View all events'}
-                            variant="orange"
-                            className="!w-full"
-                        ></Button>
-                    </a>
-                </Link>
+                        <Text variant="paragraph">Explore all our challenges</Text>
+                    </div>
+
+                    <Link
+                        href={{
+                            pathname: '/events',
+                        }}
+                        passHref
+                    >
+                        <a className="flex-1 sm:flex-none">
+                            <Button
+                                icon={TbSearch}
+                                text={'View all events'}
+                                variant="orange"
+                                className="!w-full"
+                            ></Button>
+                        </a>
+                    </Link>
+                    <div onClick={handleSendCertificates}>Test</div>
+                </div>
             </div>
-        </div>
-    </>
-);
+        </>
+    );
+}
 
 export default Home;
