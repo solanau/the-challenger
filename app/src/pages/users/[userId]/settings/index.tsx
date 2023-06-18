ac57d653f2c6357954cfcc1cc1f7f96a5ae402c8
C <caaarloos300@gmail.com>
2023-06-19 05:32:19 +0900

    Mobile friendly challenges page

--- app/src/pages/users/[userId]/settings/index.tsx
+++ app/src/pages/users/[userId]/settings/index.tsx
@@ -49,7 +49,7 @@ const UserSettingsPage: NextPage = () => {
 
     return (
         <>
-            <section className="mt-36 px-4 pt-20 sm:px-8 md:mt-0 md:px-16 lg:px-32 xl:px-48">
+            <section className="px-4 pt-20 sm:px-8 md:mt-0 md:px-16 lg:px-32 xl:px-48">
                 {!isLoggedIn && (
                     <div className="flex w-full grow flex-col items-center justify-center gap-3 p-5 text-center sm:p-8 md:px-16 lg:px-32 lg:py-16 xl:px-48 xl:py-20">
                         <Text variant="sub-heading">
