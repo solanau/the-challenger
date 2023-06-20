058f51de218b8d0050b89a11b25fd7a5006e3e47
C <caaarloos300@gmail.com>
2023-06-21 04:58:31 +0900

    Update index.tsx

--- app/src/components/home-pagev2/create-challenge/index.tsx
+++ app/src/components/home-pagev2/create-challenge/index.tsx
@@ -2,11 +2,12 @@ import Button from 'components/common/button';
 import Card from 'components/common/card';
 import Text from 'components/common/text';
 import Link from 'next/link';
+
 const CreateChallengeSection = () => {
     return (
         <section className="pt-10 pb-20 md:pt-20 md:pb-40">
             <div className="w-full max-w-6xl px-4 py-12 mx-auto text-center">
-                <Card className="p-6 bg-opacity-80 backdrop-filter backdrop-blur-md border border-gray-700 rounded-xl">
+                <Card className="p-6 bg-opacity-80 backdrop-filter backdrop-blur-md border border-gray-700 rounded-xl" style={{ backgroundColor: "zinc-900" }}>
                     <Text variant="big-heading" className="text-white mb-6">
                         Create your Own Challenge
                     </Text>
@@ -18,6 +19,7 @@ const CreateChallengeSection = () => {
                             text="Create your Own Challenge"
                             variant="transparent"
                             className="w-full sm:w-auto mx-auto hover:bg-gradient-to-br hover:from-purple-500 hover:to-indigo-500 bg-gradient-to-br from-indigo-500 to-fuchsia-500"
+                            style={{ opacity: "0.9" }}
                         />
                     </Link>
                 </Card>
@@ -25,4 +27,5 @@ const CreateChallengeSection = () => {
         </section>
     );
 };
+
 export default CreateChallengeSection;
