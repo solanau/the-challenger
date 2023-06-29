3d654fea62d7e718364f05e214f9de2bf01a0343
C <caaarloos300@gmail.com>
2023-06-30 03:57:08 +0900

    Create your own challenge improvements mobile + general

--- app/src/components/home-pagev2/create-challenge/index.tsx
+++ app/src/components/home-pagev2/create-challenge/index.tsx
@@ -5,12 +5,12 @@ import Link from 'next/link';
 
 const CreateChallengeSection = () => {
     return (
-        <section className="pt-10 pb-20 mt-70 md:pt-20 md:pb-20 relative">
+        <section className="pt-10 pb-20 mt-10 md:pt-20 md:pb-20 relative">
             <div className="w-full max-w-6xl px-4 py-12 mx-auto text-center">
-                <Card className="flex-grow p-8 bg-zinc-900 shadow-xl rounded-xl bg-opacity-70 border-zinc-600 border mx-20 ">
-                    <Text variant="big-heading" className="text-white mb-6">
-                        Create your Own Challenges
-                    </Text>
+                <Text variant="big-heading" className="text-white mb-6">
+                    Create your Own Challenge
+                </Text>
+                <Card className="flex-grow p-8 bg-zinc-900 shadow-xl rounded-xl bg-opacity-30 border-zinc-600 border mx-auto sm:w-full md:w-10/12 lg:w-9/12 xl:w-8/12">
                     <Text variant="paragraph" className="text-gray-300 mb-6">
                         The Challenger is a project run by Solana Foundation that aims to engage and educate participants in events. It offers a competition during the events where you can compete for prizes by completing different challenges. 🏆
                     </Text>
@@ -18,7 +18,7 @@ const CreateChallengeSection = () => {
                         <Button
                             text="Create your Own Challenge"
                             variant="transparent"
-                            className="w-full sm:w-auto mx-auto hover:bg-gradient-to-br hover:from-purple-500 hover:to-indigo-500 bg-gradient-to-br from-indigo-500 to-fuchsia-500"
+                            className="w-full mx-auto sm:w-auto hover:bg-gradient-to-br hover:from-purple-500 hover:to-indigo-500 bg-gradient-to-br from-indigo-500 to-fuchsia-500"
                         />
                     </Link>
                 </Card>
@@ -41,4 +41,5 @@ const CreateChallengeSection = () => {
 };
 
 export default CreateChallengeSection;
+
 
